6fb1c02a37e6a0e491b0c0dd3d21c4a0452abcde
wsprung <whitneykin@gmail.com>
2021-09-20 09:11:06 -0400

    adds YayArray file

---
+++ YayArrays.java
@@ -0,0 +1,6 @@
+public class YayArrays {
+  //different colors show us what the different pieces of our code are
+  public static void main(String[] args) {
+   System.out.println("Hello world");
+  }
+}
